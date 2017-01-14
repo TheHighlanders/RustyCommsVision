14c16ca675385611e07cd68b9c2aa1724e7ef8a0
David Matthews <david.matthews@mosaicsystems.com>
2017-01-13 16:27:02 -0800

    an initial opencv python program that filters for the color blue and does some other pre-processing and then finds and draws contours on one of the frames. TODO: change the exposure mode on the Axis M10 series cameras to be 'motion', and to be ~28 or so; not 50, this will prevent motion blur and over exposure. Then using GRIP determine what HSV thresholding is good for identifying the targets. after that do some post-processing on the contours to determine where our target is and where the robot is; see our wiki and the comments in identifyTargets.py for more details

---
+++ Python/identifyTargets.py
@@ -0,0 +1,61 @@
+import cv2
+import numpy as np
+
+
+cap = cv2.VideoCapture(0)
+
+while (True):
+	ret, frame = cap.read()
+#	cv2.imshow('frame', frame)
+
+## Pre-Processing to convert RGB image to a binary image
+
+	blur = cv2.GaussianBlur(frame, (15,15),1)
+#	cv2.imshow('blur', blur)
+
+	hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+	
+## update these for the green color of our LED
+	lower_blue = np.array([87,0,0])
+	upper_blue = np.array([151,255,255])
+
+	hsvMask = cv2.inRange(hsv, lower_blue, upper_blue)
+	cv2.imshow('mask', hsvMask)
+	
+	kernel = np.ones((15,15), np.uint8)
+	maskRemoveNoise = cv2.morphologyEx(hsvMask, cv2.MORPH_OPEN, kernel)
+	cv2.imshow('removenoise', maskRemoveNoise)
+
+	maskCloseHoles = cv2.morphologyEx(maskRemoveNoise, cv2.MORPH_CLOSE, kernel)
+	cv2.imshow('closeHoles', maskCloseHoles)
+
+	maskBlur = cv2.GaussianBlur(maskCloseHoles, (15,15),1)
+	cv2.imshow('maskBlur', maskBlur)
+## get contours for more abstract analysis
+
+	c1, hsvContours, _ = cv2.findContours(maskBlur, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+## Display all contours found;
+
+	frameContours = np.copy(frame)
+	cv2.drawContours(frameContours, hsvContours, -1, (0,0,255), 4)
+
+	cv2.imshow("frameContours", frameContours)
+
+
+## Filter contors for ones with resonable aspect ratios
+## match each filtered contour with eachother and find the set that has the highest score
+## Score dependent on 
+## * ratio of height to horizontal distance between the geometric center of each. 
+## * Area of contour
+## * distance from the center of field of each contour
+
+
+
+## Graceful shutdown	
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+## cleanup on shutdown
+cap.release();
+cv2.destroyAllWindows();
