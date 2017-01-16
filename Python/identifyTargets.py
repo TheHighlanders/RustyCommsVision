4527b8543848f395d318e37b625676a4726ec9fe
David Matthews <david.matthews@mosaicsystems.com>
2017-01-16 11:26:55 -0800

    initial draft of target identifier; lots of work todo: scoreing targets to only display 1, improving pre-proccessing to improve abilty to find targets; real world testing with the actual cameras to calibrate for distortion, etc. ,

--- Python/identifyTargets.py
+++ Python/identifyTargets.py
@@ -1,6 +1,34 @@
 import cv2
 import numpy as np
 
+def aspectRatio(w, h):
+	''' returns true if the rectangle is of the correct aspect ratio and false if not.'''	
+	return (w/h >= 1.5/5 and w/h <= 2.5/5)
+def percentFilled(w,h,cnt):
+	''' returns if the contour mostly occupies the same area as it's bounding rectangle atleast 70% '''
+	return (cv2.contourArea(cnt) >= 0.7*w*h)
+
+def correctSize(cntA, cntB):
+	'''returns true if the two contours are of similar height and false if not. bbcc testing aspect ratio before, we do not need to compare their widths'''
+	return (1.2 * cntA[3] > cntB[3] or 1.2 * cntB[3] > cntA[3])
+
+def mean(a,b):
+	'''returns the mean of two numbers'''
+	return (0.5*a + 0.5*b)
+
+def correctSpacing(cntA, cntB):
+	'''returns true if the two contours are the expected distance apart and false if not'''
+	# Expected distance
+	eDist = (mean(cntA[3], cntB[3]) / 5) * 8.25
+	#print ("eDist: ")
+	#print (eDist)
+
+	# real distance
+	rDist = abs(cntA[0] - cntB[0])
+	#print ("rDist: ")
+	#print (rDist)
+	#print ("")
+	return (rDist < 1.2 * eDist and rDist > 0.8 * eDist)
 
 #cap = cv2.VideoCapture('http://10.62.1.43/mjpg/video.mjpg')
 cap = cv2.VideoCapture(0)
@@ -17,7 +45,7 @@ while (True):
 	hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
 	
 ## update these for the green color of our LED
-	lower_green = np.array([0,0,156])
+	lower_green = np.array([0,0,75])
 	upper_green = np.array([120,209,255])
 
 	hsvMask = cv2.inRange(hsv, lower_green, upper_green)
@@ -30,16 +58,11 @@ while (True):
 	maskCloseHoles = cv2.morphologyEx(maskRemoveNoise, cv2.MORPH_CLOSE, kernel)
 	cv2.imshow('closeHoles', maskCloseHoles)
 
-#	maskBlur = cv2.GaussianBlur(maskCloseHoles, (15,15),1)
-#	cv2.imshow('maskBlur', maskBlur)
 ## get contours for more abstract analysis
 
 	c1, hsvContours, _ = cv2.findContours(maskCloseHoles, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-## Display all contours found;
-
-	frameContours = np.copy(frame)
-
+	
 
 
 ## Filter contors for ones with resonable aspect ratios
@@ -50,27 +73,33 @@ while (True):
 ## * distance from the center of field of each contour
 
 
-	possibleGearTargetContour = []
-	possibleGTCBR = []	
-# if the conour looks like a possible piece of target tape, add it to the list
+	possibleLiftTargetContour = []
+	possibleTargetBoundingRect = []	
+
+# if the contour looks like a possible piece of target tape, add it to the list
 	for cnt in hsvContours:
 		x, y, w, h = cv2.boundingRect(cnt)
-		print (w/h)
-		if (w/h >= 1.5/5 and w/h <= 2.5/5):
-			possibleGearTargetContour.append(cnt)
-			possibleGTCBR.append([x,y,w,h])
-	cv2.drawContours(frameContours, possibleGearTargetContour, -1, (0,0,255), 4)
-	cv2.imshow("frameContours", frameContours)
+		if (aspectRatio(w,h) and percentFilled(w,h,cnt)):
+			possibleLiftTargetContour.append(cnt)
+			possibleTargetBoundingRect.append([x,y,w,h])
 
-# for each contour check if there is another contour an appropiate distance away on the left or right
+## Display the contours that might be targets.
+	frameContours = np.copy(frame)
+	cv2.drawContours(frameContours, possibleLiftTargetContour, -1, (0,0,255), 4)
+	cv2.imshow("potential target half's", frameContours)
 
-	for cntA in possibleGTCBR:
-		for cntB in possibleGTCBR:
-			if cntA[3] * 2.2 >= (abs(cntA[0] - cntB[0])) and  cntA[3] * 1.8 <= (abs(cntA[0] - cntB[0])):
+# TODO: possibly update to rate the probability of each set of contours being a target, and then pick the best over a certian threshold. this would help in the case that there are two "targets" being picked up.
+
+# for each contour check if there is another similar contour an appropiate distance away on the left or right
+
+	for cntA in possibleTargetBoundingRect:
+		for cntB in possibleTargetBoundingRect:
+			if correctSize(cntA, cntB) and correctSpacing(cntA, cntB):
+				print("target found!")
 				cv2.line(frame, (cntA[0], cntA[1]), (cntB[0],cntB[1]), (0,0,255), 3)
 				cv2.circle(frame, ((int)((cntA[0] + cntB[0])/ 2),(int)((cntA[1] + cntB[1])/ 2)), (10), (0,255,255), -1)
 
-	cv2.imshow('frame', frame)
+				cv2.imshow('Target\'s aquired', frame)
 
 ## Draw a line between the targets, and put a dot at the center
 ## cv2.line(img, (startX, startY), (endX,endY), (0,0,255), thickness)
