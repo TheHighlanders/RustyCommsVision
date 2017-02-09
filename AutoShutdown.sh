6ebf80ab032114977ad62ce76b0cb6d8e4ce8a27
David Matthews <david.matthews@mosaicsystems.com>
2017-02-08 19:41:27 -0500

    creating auto shutdown deamon. to be moved to initd.

--- AutoShutdown.sh
+++ AutoShutdown.sh
@@ -1,17 +1,17 @@
 #! /bin/sh
 ### BEGIN INIT INFO
-# Provides:          StartVisionFRC6201
+# Provides:          AutoShutdownFRC6201
 # Required-Start:    $local_fs $network
 # Required-Stop:     $local_fs $network
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: FRC 6201 autostarts vision code on boot.
+# Short-Description: FRC 6201 autostarts  java demon to auto shutdown at end of match. 
 # Description:       See https://www.github.com/TheHighlanders/RustyCommsVision for more details.
 ### END INIT INFO
 
 case "$1" in
   start|"")
-	/usr/bin/taskset 0x4 /home/ubuntu/RustyCommsVision/Python/identifyTargets.py &
+	/usr/bin/java -jar /home/ubuntu/RustyCommsVision/AutoShutdown.jar
 	;;
   restart|reload|force-reload)
 	echo "Error: argument '$1' not supported" >&2s
