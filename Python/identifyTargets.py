c3fcc360c91755ccf9e927e49e0e57b8431e0d7f
David Matthews <david.matthews@mosaicsystems.com>
2017-02-09 20:04:33 -0500

    more reliable target tracking at long distances

--- Python/identifyTargets.py
+++ Python/identifyTargets.py
@@ -8,18 +8,16 @@ import subprocess
 #Created by Adriana Massie and David Matthews
 
 
-
-
 def uptime():
     ms = time.time()*1000.0 
     return ms
 
 def aspectRatio(w, h):
 	''' returns true if the rectangle is of the correct aspect ratio and false if not.'''	
-	return (w/h >= 1.5/5 and w/h <= 3/5)
+	return (w/h >= 0.8/5 and w/h <= 3/5)
 def percentFilled(w,h,cnt):
 	''' returns if the contour mostly occupies the same area as it's bounding rectangle atleast 70% '''
-	return (cv2.contourArea(cnt) >= 0.6 * w * h)
+	return (cv2.contourArea(cnt) >= 0.3 * w * h)
 
 	#cntA and cntB are contour A and B
 def correctSize(cntA, cntB):
@@ -28,7 +26,7 @@ def correctSize(cntA, cntB):
 	rawError = abs (cntA[3] - cntB[3])
 	scaledError = int (rawError / avgHeight) 
 	returnError =  int((100 / (1 + math.e** (-20*(2*scaledError - 0.7)))) + 100*scaledError)
-	print ("correctSize: " + str(returnError))
+#	print ("correctSize: " + str(returnError))
 	return (returnError) 
 
 def correctSpacingY(cntA, cntB):
@@ -44,7 +42,7 @@ def correctSpacingY(cntA, cntB):
 	rawError = abs(eDist - rDist)
 	scaledError = rawError / avgHeight
 	returnError = ((100 / (1 + math.e** (-20*(2*scaledError - 0.7)))) + 100*scaledError)
-	print ("correctYSpacing: " + str(returnError))
+#	print ("correctYSpacing: " + str(returnError))
 	return (returnError)
 
 def mean(a,b):
@@ -62,7 +60,7 @@ def correctSpacingX(cntA, cntB):
 	rawError = abs(eDist - rDist)
 	scaledError = rawError / eDist
 	returnError = ((100 / (1 + math.e** (-20*(2*scaledError - 0.7)))) + 100*scaledError)
-	print ("correctXSpacing: " + str(returnError))
+#	print ("correctXSpacing: " + str(returnError))
 	return (returnError)
 	
 def udpBroadcast (cntA, cntB):
@@ -80,6 +78,9 @@ def udpBroadcast (cntA, cntB):
 	 targetY = (targetY / capHeight)
 	 
 	 avgHeight = (avgHeight / capHeight)
+	 print(avgHeight)
+	 if (avgHeight >= 0.29):
+		 print('\n\n\n\n\n\n\n\n\n')
 	 avgWidth = (avgWidth / capWidth)
 	 bytes = bytes = str.encode((str(targetX)+ ','+str(targetY)+ ','+ str(avgWidth)+',' + str(avgHeight)))
 	 socketout.sendto(bytes,(UDP_IP,UDP_PORT)) 
@@ -106,7 +107,7 @@ def drawTarget(rectangle1: list, rectangle2: list):
 	 
 highestTargetScoreYet = 0;
 UDP_IP = '255.255.255.255' 
-UDP_PORT = 5801 
+UDP_PORT = 5801
 
 socketout = socket.socket (socket.AF_INET, socket.SOCK_DGRAM)
 socketout.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
@@ -131,11 +132,13 @@ cnt = 0
 ocnt = 0
 oinit = uptime ()
 
-print ('starting to attempt to ping the axis camera')
-p = subprocess.Popen(['ping', '-c', '2', 'https://02axis6201.local'])
+print ('beginning pinging')
+p = subprocess.Popen(['ping', '-c', '2', '10.62.1.11'])
 p.wait()
 print ('pinging completed')
-cap = cv2.VideoCapture("http://02axis6201.local/mjpg/video.mjpg")
+
+cap = cv2.VideoCapture("https://10.62.1.11/mjpg/video.mjpg")
+
 #cap = cv2.VideoCapture(1)
 print ('capture obtained')
 
@@ -144,30 +147,17 @@ print(capWidth)
 capHeight = cap.get(4)
 print(capHeight)
 
-init = uptime()
-print( 'Init:\t\t' + str(init - oinit))
 meow = 0
-while (meow <100000):
-	oimg = uptime()
-	
-	
 
-	
+while (True):
+	try:
 
-	#init = uptime()
-	#print( 'Init:\t\t' + str(init - oinit))
-	#oinit = init
-
-	meow = meow+1	
-	oread = uptime()
-	ret, frame = cap.read()
-	read = uptime()
-	print( 'read:\t\t'  + str(read - oread))
-#	cv2.imshow('frame', frame)
-	if (meow % 5 != 0 ):
+		ret, frame = cap.read()
+	except Exception as e:
 		continue
-	
-	
+
+	if (meow % 3 != 0 ):
+		continue
 
 ## Pre-Processing to convert RGB image to a binary image
 #	oblur = uptime()
@@ -178,24 +168,18 @@ while (meow <100000):
 
 
 # HSV
-	ohsv = uptime()
 	hsvi = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 	
-	hsv = uptime()
-	print( 'HSV:\t\t' + str(hsv - ohsv))
-	
+
 ## update these for the green color of our LED
 	
 	lower_green = np.array([8,0,200])
 	upper_green = np.array([180,255,255])
 
 	#This is inverted but it works on robot
-	omask = uptime()
 	hsvMask = cv2.inRange(hsvi, lower_green, upper_green)
 #	cv2.imshow('mask', hsvMask)
-	mask = uptime()
-	print( 'mask:\t\t' + str(mask - omask))
-	
+
 #	odenoise = uptime()
 	
 #	kernel = np.ones((5,5), np.uint8)
@@ -208,12 +192,8 @@ while (meow <100000):
 #	print( 'denoise:\t' + str(denoise - odenoise))
 #
 ## get contours for more abstract analysis
-	ocnt = uptime()
 
 	c1, hsvContours, _ = cv2.findContours(hsvMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-	cnt = uptime()
-	print( 'cnt:\t\t' + str(cnt - ocnt))
 
 
 ## Filter contors for ones with resonable aspect ratios
@@ -228,7 +208,6 @@ while (meow <100000):
 	possibleTargetBoundingRect = []	
 
 # if the contour looks like a possible piece of target tape, add it to the list
-	otape = uptime()
 	for cnt in hsvContours:
 		x, y, w, h = cv2.boundingRect(cnt)
 		if (aspectRatio(w,h) and percentFilled(w,h,cnt)):
@@ -236,13 +215,10 @@ while (meow <100000):
 			possibleTargetBoundingRect.append([x,y,w,h])
 	if (int(len(possibleTargetBoundingRect)) >= 1):
 		print (len(possibleTargetBoundingRect))
-	tape = uptime()
-	print( 'tape:\t\t'+ str(tape - otape))
-
 
 ## Display the contours that might be targets.
-	frameContours = np.copy(frame)
-	cv2.drawContours(frameContours, possibleLiftTargetContour, -1, (0,0,255), 4)
+#	frameContours = np.copy(frame)
+#	cv2.drawContours(frameContours, possibleLiftTargetContour, -1, (0,0,255), 4)
 #	cv2.imshow("potential target half's", frameContours)
 
 # TODO: possibly update to rate the probability of each set of contours being a target, and then pick the best over a certian threshold. this would help in the case that there are two "targets" being picked up.
@@ -254,7 +230,7 @@ while (meow <100000):
 	for cntA in possibleTargetBoundingRect:
 		for cntB in possibleTargetBoundingRect:
 			currentScore = correctSize(cntA, cntB) + correctSpacingX(cntA, cntB) + correctSpacingY(cntA, cntB)
-			print ("Score: " + str(currentScore))	
+#			print ("Score: " + str(currentScore))	
 			if currentScore < highestScore:
 				bestFoundTarget [0] = cntA
 				bestFoundTarget [1] = cntB 
@@ -263,20 +239,20 @@ while (meow <100000):
 	if (highestScore < 50): 	
 		print("target found. Score: \n\n\n\n\n " + str(highestScore))
 		udpBroadcast(bestFoundTarget[0], bestFoundTarget[1])
-		drawTarget(bestFoundTarget[0], bestFoundTarget[1])
+#		drawTarget(bestFoundTarget[0], bestFoundTarget[1])
 		if (highestScore > highestTargetScoreYet):
 			highestTargetScoreYet = highestScore
-			print("HighestScore: " +str(highestTargetScoreYet))	
-			print( 'target:\t'+ str(uptime()))
+#			print("HighestScore: " +str(highestTargetScoreYet))	
+#			print( 'target:\t'+ str(uptime()))
 	
 ## Draw a line between the targets, and put a dot at the center
 ## cv2.line(img, (startX, startY), (endX,endY), (0,0,255), thickness)
 ## cv2.circle(img, (x,y), (radius), (0,255,255), thickness)
 
-
-	img = uptime()
-	print ('img:\t\t' + str(img - oimg))
-	print ('\n\n\n')
+#
+#	img = uptime()
+#	print ('img:\t\t' + str(img - oimg))
+#	print ('\n\n\n')
 ## Graceful shutdown	
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
