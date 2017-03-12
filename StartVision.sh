1a5673392bfac55437875d5d533bb732b62959ac
David Matthews <david.matthews@mosaicsystems.com>
2017-03-12 08:41:22 -0400

    possible auto restart vision code. to be tested

--- StartVision.sh
+++ StartVision.sh
@@ -3,21 +3,24 @@
 
 case "$1" in
   start|"")
-	cd /home/frc6201/RustyCommsVision/Python/
-	
-	res="1"
-#	echo "$res"
-	while [ "$res" != "0" ]
+	while [ 1 = 1 ]
 	do
-		ping -c 1 10.62.1.11
-		res="$?"
-#		echo "$res"
+		cd /home/frc6201/RustyCommsVision/Python/
 	
+		res="1"
+	#	echo "$res"
+		while [ "$res" != "0" ]
+		do
+			ping -c 1 10.62.1.11
+			res="$?"
+	#		echo "$res"
+	
+		done
+
+		echo "starting vision"
+
+		./identifyTargets.py &
 	done
-
-	echo "starting vision"
-
-	./identifyTargets.py &
 	;;
   restart|reload|force-reload)
 	echo "Error: argument '$1' not supported" >&2s
